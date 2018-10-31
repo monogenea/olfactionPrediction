768a739764bc11e22e2aa079381b7cb4757b2388
= <=>
2018-10-31 16:17:58 +0000

    Use different dilution, median per compound

--- DREAM.R
+++ DREAM.R
@@ -19,32 +19,28 @@ download.file(paste0(ghurl, "molecular_descriptors_data.txt"),
 # Read files with readr, select least diluted compounds            
 responses <- read_tsv("data/trainSet.txt") %>%
       rename(CID = `Compound Identifier`) %>%
-      filter(Dilution == "1/10") # take the least diluted
+      filter(Dilution == "1/1,000") # take one dilution
 molFeats <- read_tsv("data/DRAGON.txt") # molecular features
 
 # Determine intersection of compounds in features and responses
 commonMols <- intersect(responses$CID,
-                             molFeats$CID)
+                        molFeats$CID)
 # Subset features and responses accordingly
 responses %<>% filter(CID %in% commonMols)
 molFeats %<>% filter(CID %in% commonMols)
 
-# Compute median/mean pleasantness across the population
-meanPlsnt <- responses %>% 
+# Compute median pleasantness across the population
+medianPlsnt <- responses %>% 
       group_by(CID) %>% 
       summarise(pleasantness = median(`VALENCE/PLEASANTNESS`, na.rm = T))
-all(meanPlsnt$CID == molFeats$CID) # TRUE - rownames match
+all(medianPlsnt$CID == molFeats$CID) # TRUE - rownames match
 
 # Concatenate predictors (molFeats) and population pleasantness
-X <-  mutate(molFeats, Y = meanPlsnt$pleasantness) %>% 
+X <-  mutate(molFeats, Y = medianPlsnt$pleasantness) %>% 
       select(-CID)
 
 # Filter nzv
 X <- X[,-nearZeroVar(X, freqCut = 4)] # == 80/20
-
-### DEBUG
-X <- select(X, 1:1000, Y)
-#####
 
 # Split train/test with rsample
 set.seed(100)
@@ -52,9 +48,9 @@ initSplit <- initial_split(X, prop = .9)
 trainSet <- training(initSplit)
 testSet <- testing(initSplit)
 
-# Create 10x 3-fold cross-validation, convert to caret class
+# Create 5-fold cross-validation, convert to caret class
 set.seed(100)
-myFolds <- vfold_cv(trainSet, v = 3, repeats = 10) %>% 
+myFolds <- vfold_cv(trainSet, v = 5) %>% 
       rsample2caret()
 ctrl <- trainControl(method = "cv")
 ctrl$index <- myFolds$index
@@ -62,33 +58,54 @@ ctrl$indexOut <- myFolds$indexOut
 
 # binary vars
 binVars <- which(sapply(X, function(x){all(x %in% 0:1)}))
+missingVars <- which(apply(X, 2, function(k){any(is.na(k))}))
 
 # Design recipe
 myRec <- recipe(Y ~ ., data = trainSet) %>% 
       step_YeoJohnson(all_predictors(), -binVars) %>% 
       step_center(all_predictors(), -binVars) %>% 
       step_scale(all_predictors(), -binVars) %>% 
-      step_knnimpute(all_numeric(), K = 5)
+      step_meanimpute(missingVars)
 
-test <- prep(myRec, training = trainSet, retain = T)
-test <- juice(test)
-
+# simple PCA, plot
 pcaRec <- myRec %>% 
-      step_kpca(all_predictors(), options = list(kernel = "rbfdot",
-                                                 kpar = list(sigma = .005)))
+      step_pca(all_predictors())
 
-myPCA <- prep(pcaRec, training = trainSet, retain = T)
-myPCA <- juice(myPCA)
-plot(myPCA$kPC1, myPCA$kPC2, cex = trainSet$Y/max(trainSet$Y) + .5,
-     col = rgb(0,0,0,.25), pch = 16)
+myPCA <- prep(pcaRec, training = trainSet, retain = T) %>% 
+      juice()
+colGrad <- trainSet$Y/100 # add color
+plot(myPCA$PC1, myPCA$PC2,
+     col = rgb(1 - colGrad, 0, colGrad,.5),
+     pch = 16, xlab = "PC1", ylab = "PC2")
+legend("topleft", pch = 16,
+       col = rgb(c(0,.5,1), 0, c(1,.5,0), alpha = .5), 
+       legend = c("Pleasant", "Neutral", "Unpleasant"), bty = "n")
 
-# Train PLS model
+# Train
 doMC::registerDoMC(10)
-fdaMod <- train(myRec, data = trainSet,
-            method = "rf",
-            tuneLength = 4,
-            trControl = ctrl)
 
-# Vaidate on testset
+plsMod <- train(myRec, data = trainSet,
+                method = "pls",
+                tuneGrid = data.frame(ncomp = c(5, 10, 15, 20, 30)),
+                trControl = ctrl)
+
+rfMod <- train(myRec, data = trainSet,
+               method = "ranger",
+               tuneGrid = expand.grid(mtry = seq(10, 2000, length.out = 5),
+                                      splitrule = c("variance", "extratrees"),
+                                      min.node.size = c(5, 10)),
+               num.trees = 1000,
+               trControl = ctrl)
+
+svmMod <- train(myRec, data = trainSet,
+                method = "svmPoly",
+                tuneLength = 3,
+                trControl = ctrl)
+
+# Validate on testset
 preds <- predict(plsMod, newdata = testSet)
-plot(preds, testSet$Y)
+plot(preds, testSet$Y,
+     xlim = c(25,80), ylim = c(25,80),
+     xlab = "Predicted", ylab = "Observed")
+abline(a=0, b=1)
+
