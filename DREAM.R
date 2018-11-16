48a564c5e2a5421a96d04894d90db347913dc7fd
Francisco Lima <franciscolima@Franciscos-iMac-Pro.local>
2018-11-16 17:57:18 +0000

    final update

--- DREAM.R
+++ DREAM.R
@@ -14,13 +14,13 @@ ghurl <- "https://github.com/dream-olfaction/olfaction-prediction/raw/master/dat
 download.file(paste0(ghurl, "TrainSet.txt"),
               destfile = "data/trainSet.txt")
 download.file(paste0(ghurl, "molecular_descriptors_data.txt"),
-              destfile = "data/DRAGON.txt")
+              destfile = "data/molFeats.txt")
 
 # Read files with readr, select least diluted compounds            
 responses <- read_tsv("data/trainSet.txt") %>%
-      rename(CID = `Compound Identifier`) %>%
-      filter(Dilution == "1/1,000") # take one dilution
-molFeats <- read_tsv("data/DRAGON.txt") # molecular features
+      rename(CID = `Compound Identifier`)
+
+molFeats <- read_tsv("data/molFeats.txt") # molecular features
 
 # Determine intersection of compounds in features and responses
 commonMols <- intersect(responses$CID,
@@ -32,7 +32,7 @@ molFeats %<>% filter(CID %in% commonMols)
 # Compute median pleasantness across the population
 medianPlsnt <- responses %>% 
       group_by(CID) %>% 
-      summarise(pleasantness = median(`VALENCE/PLEASANTNESS`, na.rm = T))
+      dplyr::summarise(pleasantness = median(`VALENCE/PLEASANTNESS`, na.rm = T))
 all(medianPlsnt$CID == molFeats$CID) # TRUE - rownames match
 
 # Concatenate predictors (molFeats) and population pleasantness
@@ -43,15 +43,15 @@ X <-  mutate(molFeats, Y = medianPlsnt$pleasantness) %>%
 X <- X[,-nearZeroVar(X, freqCut = 4)] # == 80/20
 
 # Split train/test with rsample
-set.seed(99)
+set.seed(100)
 initSplit <- initial_split(X, prop = .9,
                            strata = "Y")
 trainSet <- training(initSplit)
 testSet <- testing(initSplit)
 
 # Create 5-fold cross-validation, convert to caret class
-set.seed(99)
-myFolds <- vfold_cv(trainSet, v = 5, repeats = 3,
+set.seed(100)
+myFolds <- vfold_cv(trainSet, v = 5, repeats = 5,
                     strata = "Y") %>% 
       rsample2caret()
 ctrl <- trainControl(method = "cv",
@@ -77,6 +77,7 @@ pcaRec <- myRec %>%
 myPCA <- prep(pcaRec, training = trainSet, retain = T) %>% 
       juice()
 colGrad <- trainSet$Y/100 # add color
+
 plot(myPCA$PC1, myPCA$PC2,
      col = rgb(1 - colGrad, 0, colGrad,.5),
      pch = 16, xlab = "PC1", ylab = "PC2")
@@ -85,41 +86,58 @@ legend("topleft", pch = 16,
        legend = c("Pleasant", "Neutral", "Unpleasant"), bty = "n")
 
 # Train
-doMC::registerDoMC(6)
-plsMod <- train(myRec, data = trainSet,
-                method = "pls",
-                tuneGrid = data.frame(ncomp = 1:10),
+doMC::registerDoMC(10)
+knnMod <- train(myRec, data = trainSet,
+           method = "knn",
+           tuneGrid = data.frame(k = seq(5, 25, by = 4)),
+           trControl = ctrl)
+
+enetMod <- train(myRec, data = trainSet,
+                 method = "glmnet",
+                 tuneGrid = expand.grid(alpha = seq(0, 1, length.out = 5),
+                                        lambda = seq(.5, 2, length.out = 5)),
+                 trControl = ctrl)
+
+svmMod <- train(myRec, data = trainSet,
+                method = "svmRadial",
+                tuneLength = 8,
                 trControl = ctrl)
 
 rfMod <- train(myRec, data = trainSet,
                method = "ranger",
-               tuneGrid = expand.grid(mtry = seq(10, 2000, length.out = 5),
-                                      splitrule = c("variance", "extratrees"),
-                                      min.node.size = c(5, 10)),
-               num.trees = 1000,
+               tuneLength = 8,
+               num.trees = 5000,
                trControl = ctrl)
 
-svmMod <- train(myRec, data = trainSet,
-                method = "svmRadial",
-                tuneLength = 6,
-                trControl = ctrl)
-
-xgboost <- train(myRec, data = trainSet,
+xgbMod <- train(myRec, data = trainSet,
                 method = "xgbTree",
-                tuneLength = 4,
+                tuneLength = 8,
                 trControl = ctrl)
 
-bwplot(resamples(list("PLS" = plsMod,
-                      "RF" = rfMod,
-                      "SVM" = svmMod,
-                      "XGBOOST" = xgboost)),
-       metric = "MAE")
+cubMod <- train(myRec, data = trainSet,
+                method = "cubist",
+                tuneLength = 8,
+                trControl = ctrl)
 
-# Validate on testset
-preds <- predict(plsMod, newdata = testSet)
-plot(preds, testSet$Y,
+modelList <- list("KNN" = knnMod,
+                  "ENET" = enetMod,
+                  "SVM" = svmMod,
+                  "RF" = rfMod,
+                  "XGB" = xgbMod,
+                  "CUB" = cubMod)
+
+bwplot(resamples(modelList),
+       metric = "RMSE")
+
+# Validate on test set with ensemble
+allPreds <- sapply(modelList, predict, newdata = testSet)
+ensemblePred <- rowSums(allPreds) / length(modelList)
+
+# Plot predicted vs. observed; create PNG
+plot(ensemblePred, testSet$Y,
      xlim = c(0,100), ylim = c(0,100),
      xlab = "Predicted", ylab = "Observed",
      pch = 16, col = rgb(0, 0, 0, .25))
 abline(a=0, b=1)
 
+writeLines(capture.output(sessionInfo()), "sessionInfo")
